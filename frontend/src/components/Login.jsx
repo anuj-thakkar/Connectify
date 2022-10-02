aa8cce98c1ef182b062a8ab0bf0932be441bbc03
Rama Deshpande <rama@deshpandez.com>
2022-10-01 22:10:54 -0400

    login page updated ui

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import fire from '../fire.js';
+import logo from './logo.jpg';
 
 const Login = () => {
     const [email, setEmail] = useState();
@@ -22,47 +23,52 @@ const Login = () => {
     }
 
     return (
-        <div>    
-        <h2>Login</h2>
-            <form onSubmit={handleLogin}>
-                <input
-                    type="text"
-                    onChange={({ target }) =>     
-                      setEmail(target.value)}
-                    placeholder="Email"
-                />
-                <br />
-                <input
-                    type="password"
-                    onChange={({ target}) => 
-                      setPassword(target.value)}
-                    placeholder="Password"
-                />
-                <br />
-                <button type="submit">
-                    Sign in
-                </button>
-            </form>
-        <h2>Register</h2>
-            <form onSubmit={handleRegister}>
-                <input
-                    type="text"
-                    onChange={({ target }) =>     
-                      setEmail(target.value)}
-                    placeholder="Email"
-                />
-                <br />
-                <input
-                    type="password"
-                    onChange={({ target}) => 
-                      setPassword(target.value)}
-                    placeholder="Password"
-                />
-                <br />
-                <button type="submit">
-                    Sign in
-                </button>
-            </form>
+            <div class="logoDiv">
+                <img src={logo} alt="logo"></img>
+                <div class="loginDiv">
+                    <h1 class="loginHdr">Login</h1>
+                    <form onSubmit={handleLogin}>
+                        <input
+                            type="text"
+                            onChange={({ target }) =>     
+                            setEmail(target.value)}
+                            placeholder="Email"
+                        />
+                        <br />
+                        <input
+                            type="password"
+                            onChange={({ target}) => 
+                            setPassword(target.value)}
+                            placeholder="Password"
+                        />
+                        <br />
+                        <button type="submit">
+                            Sign in
+                        </button>
+                    </form>
+                </div>
+                <div class="registerDiv">
+                    <h1>Register</h1>
+                    <form onSubmit={handleRegister}>
+                        <input
+                            type="text"
+                            onChange={({ target }) =>     
+                            setEmail(target.value)}
+                            placeholder="Email"
+                        />
+                        <br />
+                        <input
+                            type="password"
+                            onChange={({ target}) => 
+                            setPassword(target.value)}
+                            placeholder="Password"
+                        />
+                        <br />
+                        <button type="reg">
+                            Register
+                        </button>
+                    </form>
+                </div>
         </div>
     )
 };
