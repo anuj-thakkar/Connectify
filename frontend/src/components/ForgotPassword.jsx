76f76a5d2df8fe82a9799669636ebbc53ad11168
ihpatel <patelishan320@gmail.com>
2022-10-02 19:28:16 -0400

    added forgot password page ui

--- frontend/src/components/ForgotPassword.jsx
+++ frontend/src/components/ForgotPassword.jsx
@@ -2,13 +2,28 @@ import React, { useState } from 'react';
 import fire from '../fire.js';
 
 const ForgotPassword = () => {
+    const [errorMessage, setErrorMessage] = useState('');
+    const [success, setSuccess] = useState(false);
+    const [emailValue, setEmailValue] = useState('');
 
 
-    return (
-            <div class="logoDiv">
-            <div class="ForgotPassword">
-
-            </div>
+    return success ? (
+        <div className="content-container">
+            <h1>Success</h1>
+            <p>Please check your email for the reset link</p>
+        </div>
+    ) : (
+        <div className="content-container">
+            <h1>Forgot Password</h1>
+            <p>Enter your email and we'll send you a reset link</p>
+            {errorMessage && <div className="fail">{errorMessage}</div>}
+            <input
+                value={emailValue}
+                onChange={e => setEmailValue(e.target.value)} 
+                placeholder="someone@gmail.com" />
+            <button
+                disabled={!emailValue}
+            >Send Reset link</button>
         </div>
     )
 };
