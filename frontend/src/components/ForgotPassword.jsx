d96026cfa2ba4c0cdf30248bcc0d19d48c04abd0
Oliver Bonilla <oliverbonilla@pal-nat186-109-15.itap.purdue.edu>
2022-10-02 17:24:03 -0400

    Addded forgot button and tried to create its link

---
+++ frontend/src/components/ForgotPassword.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import fire from '../fire.js';
+
+const ForgotPassword = () => {
+
+
+    return (
+            <div class="logoDiv">
+            <div class="ForgotPassword">
+
+            </div>
+        </div>
+    )
+};
+
+export default ForgotPassword
