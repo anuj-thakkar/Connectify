8186284337ad37c42d1f9460e503dfafb98be5cb
Rama Deshpande <rama@deshpandez.com>
2022-10-05 00:28:34 -0400

    updated Register page UI

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -14,59 +14,74 @@ const Register = () => {
             console.error('Unable to Register');
         });
     }
-
     return (
-            <div class="logoDiv">
-            <div class="Register">
-                <h1 class="registerHDR"> Enter the following details </h1>
-                <form onSubmit={handleRegister}>
-                        <input
-                            type="text"
-                            onChange={({ target }) =>     
-                            setEmail(target.value)}
-                            placeholder="First Name"
-                        />
-                        <br />
-                        <input
-                            type="text"
-                            onChange={({ target}) => 
-                            setPassword(target.value)}
-                            placeholder="Last Name"
-                        />
-                        <br />
-                        <input
-                            type="email"
-                            onChange={({ target}) => 
-                            setPassword(target.value)}
-                            placeholder="Email"
-                        />
-                        <br />
-                        <input
-                            type="password"
-                            onChange={({ target}) => 
-                            setPassword(target.value)}
-                            placeholder="Password"
-                        />
-                        <br />
-                        <input
-                            type="password"
-                            onChange={({ target}) => 
-                            setPassword(target.value)}
-                            placeholder="Confirm Password"
-                        />
-                        <br />
-                        <Link to="/">
-                            <button type="return">
-                                Go back
-                            </button>
-                        </Link>
-                        <button type="submit">
-                            Register
-                        </button>
-                    </form>
+        <div className="Auth-form-container">
+          <form className="Auth-form" onSubmit={handleRegister}>
+            <div className="Auth-form-content">
+            <h3 className="Auth-form-title">Create Account</h3>
+            <p className="text-center mt-2">
+                <a href="/">Already have an account?</a>
+            </p>
+                <div className="form-group mt-3">
+                <label>First Name</label>
+                <input
+                  type="email"
+                  className="form-control mt-1"
+                  onChange={({ target }) =>     
+                  setEmail(target.value)}
+                  placeholder="First Name"
+                />
+                </div>
+                <div className="form-group mt-3">
+                <label>Last Name</label>
+                <input
+                  type="email"
+                  className="form-control mt-1"
+                  onChange={({ target }) =>     
+                  setEmail(target.value)}
+                  placeholder="Last Name"
+                />
+                </div>
+              <div className="form-group mt-3">
+                <label>Email address</label>
+                <input
+                  type="email"
+                  className="form-control mt-1"
+                  onChange={({ target }) =>     
+                  setEmail(target.value)}
+                  placeholder="Enter a valid email"
+                />
+              </div>
+              <div className="form-group mt-3">
+                <label>Password</label>
+                <input
+                  type="password"
+                  className="form-control mt-1"
+                  onChange={({ target}) => 
+                  setPassword(target.value)}
+                  placeholder="Create a password"
+                />
+              </div>
+              <div className="form-group mt-3">
+                <label>Password</label>
+                <input
+                  type="password"
+                  className="form-control mt-1"
+                  onChange={({ target}) => 
+                  setPassword(target.value)}
+                  placeholder="Confirm password"
+                />
+              </div>
+              <div className="d-grid gap-2 mt-3">
+                <button type="submit" className="btn btn-success">
+                  Register
+                </button>
+              </div>
+              <p></p>
             </div>
+          </form>
         </div>
-    )
+      )
 };
 
 export default Register
