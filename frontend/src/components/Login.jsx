33ac5e21589d715c0647ee42484f14d282b1573e
Anuj Thakkar <anujtthakkar@gmail.com>
2022-10-12 18:19:49 -0400

    changes to register and login frontend

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,21 +1,27 @@
 import React, { useState } from 'react';
 import fire from '../fire.js';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 const Login = () => {
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
+    const [isError, setErrorMessage] = useState("");
     
     const handleLogin = (e) => {
-        e.preventDefault();
-        fire.auth().signInWithEmailAndPassword(email, password)
-          .catch((error) => {
-            console.error('Incorrect username or password');
-        });
-    }
+
+          e.preventDefault();
+          fire.auth().signInWithEmailAndPassword(email, password)
+            .catch((error) => {
+              setErrorMessage(error);
+              console.error('Incorrect username or password');
+          });
+        }
+        
+
     return (
         <div className="Auth-form-container">
-          <form className="Auth-form" onSubmit={handleLogin}>
+          <form className="Auth-form" onSubmit={handleLogin}
+          >
             <div className="Auth-form-content">
               <h3 className="Auth-form-title">Log In </h3>
               <p className="text-center mt-2">
@@ -45,8 +51,10 @@ const Login = () => {
                 <button type="submit" className="btn btn-success">
                   Submit
                 </button>
+                <p class = "error red-text center align"> </p>
+                
+
               </div>
-              <p></p>
 
               <div class="forgot-password text-right mt-2">
                   <p className="text-center mt-2">
