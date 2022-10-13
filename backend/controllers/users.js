02427e1f39157c1e9374ee8ae16c5d55312c77c1
Anuj Thakkar <anujtthakkar@gmail.com>
2022-10-13 02:04:58 -0400

    changes made to backend for uploading images

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,5 +1,12 @@
 const userRouter = require('express').Router();
 const User = require('../models/user')
+const express = require("express");
+
+const app = express();
+// module required to change User profile picture 
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
 
 userRouter.get('/', (req, res) => {
   return res.send('user router GET'); 
@@ -11,4 +18,9 @@ userRouter.post('/', (req, res) => {
   return res.status(201).json(savedUser);
 });
 
-module.exports = userRouter;
+app.get("/upload", upload.single("avatar"), (req, res) => {
+  console.log(req.file);  
+  res.send("File uploaded successfully.");
+  res.send("200")
+
+});
