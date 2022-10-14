bf209f16c3d0ea83b646bbd48c7783b94b3f82bb
OrlandoOtero02 <ouotero@purdue.edu>
2022-10-14 16:40:03 -0400

    profile Picture

--- frontend/src/components/profileInfo.jsx
+++ frontend/src/components/profileInfo.jsx
@@ -1,12 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 import fire from "../fire.js";
 import logo from "./logo.jpg";
 import { NavLink, Link } from "react-router-dom";
 import Settings from "./Settings";
 import { MdHomeFilled, MdSearch } from "react-icons/md";
 import "../App.css";
+import axios from "axios";
 
-const profileInfo = () => {
+const ProfileInfo = () => {
+  const [image, setState] = useState({});
+  const fileOnChange = (e) => {
+    console.log(e.target.files[0]);
+  };
+
+  const sendImage = (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("avater", image);
+    console.log(formData);
+
+    axios
+      .post("http://localhost:3001/api/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((res) => {
+        console.log(res.json());
+      })
+      .catch((err) => {
+        console.log(err.json());
+      });
+  };
   const signOut = () => {
     fire.auth().signOut();
   };
@@ -77,10 +103,24 @@ const profileInfo = () => {
             </div>
           </div>
         </nav>
-        <img src={require("./profileIcon.png")} class="ProfileImage" />
+
+        <div class="rowProfile">
+          <div class="columnProfile">
+            <fieldset className="form-group">
+              <label for="profilePicture" style={{ color: "white" }}>
+                Profile Picture{" "}
+              </label>
+              <input type="file" onChange={fileOnChange} />
+              <button onClick={sendImage}>Upload</button>
+            </fieldset>
+          </div>
+          <div class="columnProfile">
+            <h3>Connections</h3>
+          </div>
+        </div>
       </div>
     </>
   );
 };
 
-export default profileInfo;
+export default ProfileInfo;
