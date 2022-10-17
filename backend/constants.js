347af3a9f65f21ea1bca1f7a1690d93b06906223
Anuj Thakkar <anujtthakkar@gmail.com>
2022-10-17 15:14:55 -0400

    Initial backend reconfig

---
+++ backend/constants.js
@@ -0,0 +1,16 @@
+/**
+ * @fileoverview Store some constants regarding users.
+ */
+
+ module.exports = Object.freeze({
+    // The following are constants to be used for Users
+    U_INVALID_INPUT: 'Invalid input!',
+    U_CREATION_SUCCESS: 'User created!',
+    U_CREATION_FAILURE: 'User creation failure!',
+    U_EMAIL_TAKEN: 'Email already taken!',
+    U_LOGIN_SUCCESS: 'User login successful!',
+    U_INVALID_CREDENTIALS: 'Invalid credentials!',
+    U_UPDATE_FAILURE: 'User update failure!',
+    U_DELETION_FAILURE: 'User deletion faulure!',
+    U_DOES_NOT_EXIST: 'User does not exist!'
+});
