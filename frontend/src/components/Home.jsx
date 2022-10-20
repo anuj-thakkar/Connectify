c56ccd63a23cec75f51d14903961110c7684ffda
Oliver Bonilla <oliverbonilla@pal-nat186-101-154.itap.purdue.edu>
2022-10-19 21:49:58 -0400

    removed unused functions

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -78,57 +78,6 @@ const Home = () => {
       console.log(currentPlaying.name)
     } 
 
-    const changeState = async () => {
-      const state = playerState ? "pause" : "play";
-      await axios.put(
-        `https://api.spotify.com/v1/me/player/${state}`,
-        {},
-        {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: "Bearer " + token,
-          },
-        }
-      );
-      dispatch({
-        type: reducerCases.SET_PLAYER_STATE,
-        playerState: !playerState,
-      });
-    };
-
-    const changeTrack = async (type) => {
-      await axios.post(
-        `https://api.spotify.com/v1/me/player/${type}`,
-        {},
-        {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: "Bearer " + token,
-          },
-        }
-      );
-      dispatch({ type: reducerCases.SET_PLAYER_STATE, playerState: true });
-      const response1 = await axios.get(
-        "https://api.spotify.com/v1/me/player/currently-playing",
-        {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: "Bearer " + token,
-          },
-        }
-      );
-      if (response1.data !== "") {
-        const currentPlaying = {
-          id: response1.data.item.id,
-          name: response1.data.item.name,
-          artists: response1.data.item.artists.map((artist) => artist.name),
-          image: response1.data.item.album.images[2].url,
-        };
-        dispatch({ type: reducerCases.SET_PLAYING, currentPlaying });
-      } else {
-        dispatch({ type: reducerCases.SET_PLAYING, currentPlaying: null });
-      }
-    };
 
     return (
         <>
