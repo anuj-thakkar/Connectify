ead2b51c38583a7965b5ace67366dc16c39bd42d
Anuj Thakkar <anujtthakkar@gmail.com>
2022-10-20 02:20:55 -0400

    updateProfilePicture endpoint added

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,5 +1,5 @@
 const userRouter = require('express').Router();
-const { Router } = require('express');
+const { express } = require('express');
 const { useParams } = require('react-router-dom');
 const User = require('../models/user')
 
@@ -31,26 +31,22 @@ userRouter.post('/', (req, res) => {
 
 userRouter.post('/updateEmail', async (req, res) => {
   var params = req.body;
-  User.findOne({'email':params.email}, function(err,user) {
-    if (err) {
-      console.log(err);
-      return res.status(400).send('Error updating email');
-    } else {
-      user.email = params.email;
-      User.findOne({email: params.email}, function(err, existingUser) {
-        if(err) return next(err);
-        if(existingUser) {
-          return res.status(404).send('Account with that email already exists.');
-        } else {
-          User.updateOne({"_id":params._id},params, function(err, user) {
-            if (err) return next(err);
-            return res.status(200).send('Email address updated Successfully.');
-          });
-        }
-      });      
-    }
+  User.findOne({'email':params.email}, function(err,existingUser) {
+    user.email = params.email;
+    User.findOne({email: params.email}, function(err, existingUser) {
+      if(err) return next(err);
+      if(existingUser) {
+        return res.status(404).send('Account with that email already exists.');
+      } else {
+        User.updateOne({"email":params.email},params, function(err, user) {
+          if (err) return next(err);
+          return res.status(200).send('Email address updated Successfully.');
+        });
+      }
+    });      
   });
 });
+
 
 userRouter.post('/updateBio', async (req, res) => {
   var params = req.body;
@@ -60,7 +56,7 @@ userRouter.post('/updateBio', async (req, res) => {
       return res.status(400).send('Error updating bio');
     } else {
       user.bio = params.bio;
-      User.updateOne({"email":params.email},params, function(err, user) {
+      User.updateOne({"bio":params.bio},params, function(err, user) {
         if (err) return next(err);
         return res.status(200).send('Bio updated Successfully.');
       });
@@ -69,10 +65,34 @@ userRouter.post('/updateBio', async (req, res) => {
 });
 
 
-userRouter.post('/updateProfilePicture', async (req, res) => {
 
-  // TODO: Update profile picture
+/**
+ * packages needed to update profile picture 
+ */
+const fs = require('fs');
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
+userRouter.post('/updateProfilePicture', upload.single("avatar"), async (req, res) => {
+
+  let fileType = req.file.mimetype.split("/")[1];
+  let fileName = req.file.filename + "." + fileType;
+
+  fs.rename('./uploads/' + req.file.filename, './uploads/' + fileName, (err) => {
+    if (err) {
+      console.log("File rename error: " + err);
+    }
+    console.log("callback");
+    console.log("File renamed");
+  });
+
+  console.log("fileType: ", fileType);
+  console.log("req.file", req.file);  
+  res.send("File uploaded successfully.");
+  res.send("200")
+
 });
+
   
 
 
--- backend/models/User.js
+++ backend/models/User.js
@@ -2,22 +2,71 @@ const mongoose = require("mongoose");
 const Connections = require("./Connections");
 
 const userSchema = new mongoose.Schema({
-  username: { type: String , required: true },
-  name: { type: String , required: true },
-  email: { type: String, required: true, lowercase: true, validate: /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/, unique: true },
-  connections: { type: [{
-    username: { type: String, required: true, lowercase: true },
-    status: {
-        type: Number,
-        enums: [
-            0,    //'add friend',
-            1,    //'requested',
-            2,    //'pending',
-            3,    //'friends'
-        ]
-    }
-  }], required: true},
-  bio: { type: String, required: true},
+
+  username: { 
+    type: String , 
+    required: true 
+  },
+
+  name: { 
+    type: String,
+    required: true
+  },
+  
+  email: { 
+    type: String,
+    required: true, 
+    lowercase: true, 
+    validate: /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/, 
+    unique: true 
+  },
+
+  connections: { 
+    type: [{
+      username: { 
+        type: String, 
+        required: true, 
+        lowercase: true 
+      },
+      status: {
+          type: Number,
+          enums: [
+              0,    //'add friend',
+              1,    //'requested',
+              2,    //'pending',
+              3,    //'friends'
+          ]
+      }
+    }], 
+    required: true},
+
+  bio: { 
+    type: String, 
+    required: true
+  },
+
+  profilePicture: {
+    type: String,
+    default: "https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png"
+  },
+
+  followers: {
+    type: [{
+      username: {
+        type: String,
+        required: true,
+      }
+    }],
+  },
+
+  following: {
+    type: [{
+      username: {
+        type: String,
+        required: true,
+      }
+    }],
+  }
 
   // posts : { type: Array, required: true }
   // uid: { type: Number, required: true}
