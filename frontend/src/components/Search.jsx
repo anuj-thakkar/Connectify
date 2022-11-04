b23303d2462b84785726050626fe5805780291a6
OrlandoOtero02 <ouotero@purdue.edu>
2022-11-04 15:48:20 -0400

    can search both artists/tracks but can't display

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -28,27 +28,43 @@ const Search = () => {
         Authorization: "Bearer " + token,
       },
     };
-    var artistID = await fetch(
-      "https://api.spotify.com/v1/search?q=" + searchInput + "&type=artist",
-      searchParameters
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        return data.artists.items[0].id;
-      });
-    //Get request with Artist ID grab all the albums from that artist
-    var returnedAlbums = await fetch(
-      "https://api.spotify.com/v1/artists/" +
-        artistID +
-        "/albums?include_groups=album&market=US&limit=12",
+    // var artistID = await fetch(
+    //   "https://api.spotify.com/v1/search?q=" + searchInput + "&type=artist",
+    //   searchParameters
+    // )
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     console.log("artistID is ");
+    //     console.log(data);
+    //     return data.artists.items[0].id;
+    //   });
+    // //Get request with Artist ID grab all the albums from that artist
+    // var returnedAlbums = await fetch(
+    //   "https://api.spotify.com/v1/artists/" +
+    //     artistID +
+    //     "/albums?include_groups=album&market=US&limit=12",
+    //   searchParameters
+    // )
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     console.log("artists albums are:");
+    //     console.log(data);
+    //     setAlbums(data.items);
+    //   });
+    //Get request using search to get top tracks/artists
+    var results = await fetch(
+      "https://api.spotify.com/v1/search?q=" +
+        searchInput +
+        "&type=track,artist&limit=4",
       searchParameters
     )
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
-        setAlbums(data.items);
+        setAlbums(data.tracks.items);
+        setAlbums([...albums, data.artists.items]);
+        console.log("it worked");
       });
-    //display those albums to the user
   }
   return (
     <>
@@ -59,7 +75,7 @@ const Search = () => {
               placeholder="Search for Artist"
               type="input"
               onKeyPress={(event) => {
-                if (event.key == "Enter") {
+                if (event.key === "Enter") {
                   search();
                 }
               }}
@@ -73,7 +89,19 @@ const Search = () => {
             {albums.map((album, i) => {
               return (
                 <Card className="text-black">
-                  <Card.Img src={album.images[0].url} />
+                  {/* <Card.Img src={album.album.images[0].url} /> */}
+                  <Card.Img
+                    src={() => {
+                      console.log("getting url");
+                      if (album.album !== undefined) {
+                        console.log("track");
+                        return album.album.images[0].url;
+                      } else {
+                        console.log("artist");
+                        return album.images[0].url;
+                      }
+                    }}
+                  />
                   <Card.Body>
                     <Card.Text className="fs-6">{album.name}</Card.Text>
                   </Card.Body>
