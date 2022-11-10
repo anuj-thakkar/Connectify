b958c2d884e689357609a7127bb819995c86d46a
Oliver Bonilla <oliverbonilla@pal-10-184-60-190.itap.purdue.edu>
2022-11-10 12:51:03 -0500

    add track functional

--- frontend/src/components/Playlist.jsx
+++ frontend/src/components/Playlist.jsx
@@ -169,8 +169,25 @@ export default function Body() {
     setRemove(false)
   }
 
-  const refresh = () => {
-    window.location.reload(false);
+  const addSong = async (track) => {
+    var parameters = {
+      method: "POST", 
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+    };
+    await fetch(
+      "https://api.spotify.com/v1/playlists/" +
+        state.PlaylistId +
+        "/tracks?uris=" + track.uri,
+      parameters,
+    ).then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    }); 
+    window.location.reload(false)
   }
 
   return (
@@ -218,7 +235,7 @@ export default function Body() {
             <button type="button" onClick={() => cancelRemoveButton()} className="btn btn-outline-success">
               Cancel
             </button>
-            <div className="search">
+       <div className="search">
           <InputGroup className="mb-3" size="small">
             <FormControl
               placeholder="Search for Song"
@@ -235,7 +252,7 @@ export default function Body() {
           <Row className="mx-2 row row-cols-4">
             {albums.map((album, i) => {
               return (
-                <Card className="text-black" onClick={() => refresh()}>
+                <Card className="text-black" onClick={() => addSong(album)}>
                                     {<Card.Img src={album.album.images[0].url} /> }
                                     <Card.Img/>
                   <Card.Body>
