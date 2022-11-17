3500e3bb0f8ee7bb6b00c9bf8bfdfd987ffbe0e4
Anuj Thakkar <anujtthakkar@gmail.com>
2022-11-17 03:23:45 -0500

    CreatePosts.jsx

---
+++ frontend/src/components/CreatePosts.jsx
@@ -0,0 +1,45 @@
+import React,{useState,useEffect} from 'react'
+const CreatPost = ()=>{
+   return(
+       <div className="card input-filed"
+       style={{
+           margin:"30px auto",
+           maxWidth:"500px",
+           padding:"20px",
+           textAlign:"center"
+       }}
+       >
+           <input 
+           type="text"
+            placeholder="title"
+            value={title}
+            onChange={(e)=>setTitle(e.target.value)}
+            />
+           <input
+            type="text"
+             placeholder="body"
+             value={body}
+            onChange={(e)=>setBody(e.target.value)}
+             />
+           <div className="file-field input-field">
+            <div className="btn #64b5f6 blue darken-1">
+                <span>Uplaod Image</span>
+                <input type="file" onChange={(e)=>setImage(e.target.files[0])} />
+            </div>
+            <div className="file-path-wrapper">
+                <input className="file-path validate" type="text" />
+            </div>
+            </div>
+            <button className="btn waves-effect waves-light #64b5f6 blue darken-1"
+            onClick={()=>postDetails()}
+            
+            >
+                Submit post
+            </button>
+
+       </div>
+   )
+}
+
+
+export default CreatPost
