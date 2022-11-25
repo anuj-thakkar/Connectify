70b90e77f0a367ad2f04bdbcc029883a8fecf6da
Rama Deshpande <rama@deshpandez.com>
2022-11-25 13:02:16 -0600

    chat ui created

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,47 +1,135 @@
 import React, { useState, useEffect } from "react";
 import img from "../img.png";
 import Attach from "../attach.png";
-
+import logo from "../static/logo.jpg";
+import { useStateProvider } from "../utils/StateProvider";
+import Settings from "./Settings";
+import fire from "../fire.js";
+import {
+  MdHomeFilled,
+  MdBuild,
+  MdAccountCircle,
+  MdSearch,
+  MdCompareArrows,
+  MdChat,
+  MdAddReaction
+} from "react-icons/md";
+import "../App.css";
 
 const ChatForm = () => {
+  const { token } = useStateProvider();
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
 
-    const [message, setMessage] = useState("");
-    const [messages, setMessages] = useState([]);
-    
-    const sendMessage = (e) => {
-        e.preventDefault();
-        setMessage("");
-    };
-    
-    useEffect(() => {
-        
-    }, [messages]);
-    
-    return (
-      <div className="input">
-      <input
-        type="text"
-        placeholder="Type something..."
-        size={175}
-        //onChange={(e) => setText(e.target.value)}
-        //value={text}
-      />
-      <div className="send">
-        <img src={Attach} alt="" />
-        <input
-          type="file"
-          style={{ display: "none" }}
-          id="file"
-          //onChange={(e) => setImg(e.target.files[0])}
-        />
-        <label htmlFor="file">
-          <img src={img} alt="" />
-        </label>
-        <button type="submit">Send</button>
+  const sendMessage = (e) => {
+    e.preventDefault();
+    setMessage("");
+  };
+
+  useEffect(() => {
+
+  }, [messages]);
+
+  const signOut = () => {
+    fire.auth().signOut();
+    localStorage.clear();
+  };
+
+  return (
+    <div class="grid-container">
+      <div class="itemnav">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-black">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+              <img
+                src={logo}
+                alt=""
+                padding-left="10"
+                height="60"
+                class="d-inline-block align-text-top"
+              ></img>
+            </a>
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavAltMarkup"
+              aria-controls="navbarNavAltMarkup"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              &nbsp; &nbsp;
+              <div class="navbar-nav">
+                <a
+                  class="nav-link active"
+                  aria-current="page"
+                  href={`/home#access_token=${token}&token_type=Bearer&expires_in=3600`}
+                >
+                  <MdHomeFilled /> Home
+                </a>
+                &nbsp; &nbsp;
+                <a
+                  class="nav-link active"
+                  aria-current="page"
+                  onClick={Settings}
+                  href={`/home/settings#access_token=${token}&token_type=Bearer&expires_in=3600`}
+                >
+                  <MdBuild /> Settings
+                </a>
+                &nbsp; &nbsp;
+                <a
+                  class="nav-link active"
+                  aria-current="page"
+                  href={`../profile#access_token=${token}&token_type=Bearer&expires_in=3600`}
+                >
+                  <MdAccountCircle /> Profile
+                </a>
+                &nbsp; &nbsp;
+                <a
+                  class="nav-link active"
+                  aria-current="page"
+                  href={`../connections#access_token=${token}&token_type=Bearer&expires_in=3600`}
+                >
+                  <MdAddReaction /> Connections
+                </a>
+                &nbsp; &nbsp;
+                <a
+                  class="nav-link active"
+                  aria-current="page"
+                  //onClick={MessageForm}
+                  href={`/home/chat#access_token=${token}&token_type=Bearer&expires_in=3600`}
+                >
+                  <MdChat /> Chat
+                </a>
+                &nbsp; &nbsp;
+                <a
+                  class="nav-link active"
+                  aria-current="page"
+                  onClick={signOut}
+                  href="/#"
+                >
+                  <MdCompareArrows /> Sign out
+                </a>
+              </div>
+            </div>
+          </div>
+        </nav>
       </div>
+      <div class="itemright">
+        <fieldset class="input">
+            <label for="input" style={{ color: 'white' }}></label>
+            <input type="text" placeholder="type a chat message..." className="form-control" />
+            &nbsp; &nbsp;
+            <button className="btn btn-success">Send</button>
+        </fieldset>
+      </div>
+      <div class="itemrest"></div>
     </div>
-    );
+        );
 
 }
 
-export default ChatForm;
+        export default ChatForm;
