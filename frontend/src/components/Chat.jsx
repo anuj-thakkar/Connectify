2a2d1927d81603dcf3c2413940d9357c84917618
Oliver Bonilla <oliverbonilla@MacBook-Pro-4.local>
2022-11-27 23:43:13 -0500

    draft chats

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,41 +1,135 @@
 import React, { useState, useEffect } from "react";
-
+import logo from "../static/logo.jpg";
+import { useStateProvider } from "../utils/StateProvider";
+import Settings from "./Settings";
+import fire from "../fire.js";
+import {
+  MdHomeFilled,
+  MdBuild,
+  MdAccountCircle,
+  MdSearch,
+  MdCompareArrows,
+  MdChat,
+  MdAddReaction
+} from "react-icons/md";
+import "../App.css";
+import {ChatEngine} from 'react-chat-engine';
+import './Chat.css';
 
 const ChatForm = () => {
+  const [{ token }] = useStateProvider();
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
 
-// START OF PLACEHOLDER CODE FOR CHAT FORM
+  const sendMessage = (e) => {
+    e.preventDefault();
+    setMessage("");
+  };
 
-    const [message, setMessage] = useState("");
-    const [messages, setMessages] = useState([]);
-    
-    const sendMessage = (e) => {
-        e.preventDefault();
-        setMessage("");
-    };
-    
-    useEffect(() => {
-        
-    }, [messages]);
-    
-    return (
-        <div>
-        <form onSubmit={sendMessage}>
-            <input
-            type="text"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            />
-            <button type="submit">Send</button>
-        </form>
-        <div>
-            {messages.map((message) => (
-            <div>{message}</div>
-            ))}
-        </div>
-        </div>
-    );
+  useEffect(() => {
 
-    // END OF PLACEHOLDER CODE FOR CHAT FORM
+  }, [messages]);
+
+  const signOut = () => {
+    fire.auth().signOut();
+    localStorage.clear();
+  };
+
+  return (
+    <div class="grid-container">
+      <div class="itemnav">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-black">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+              <img
+                src={logo}
+                alt=""
+                padding-left="10"
+                height="60"
+                class="d-inline-block align-text-top"
+              ></img>
+            </a>
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavAltMarkup"
+              aria-controls="navbarNavAltMarkup"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              &nbsp; &nbsp;
+              <div class="navbar-nav">
+                <a
+                  class="nav-link active"
+                  aria-current="page"
+                  href={`/home#access_token=${token}&token_type=Bearer&expires_in=3600`}
+                >
+                  <MdHomeFilled /> Home
+                </a>
+                &nbsp; &nbsp;
+                <a
+                  class="nav-link active"
+                  aria-current="page"
+                  onClick={Settings}
+                  href={`/home/settings#access_token=${token}&token_type=Bearer&expires_in=3600`}
+                >
+                  <MdBuild /> Settings
+                </a>
+                &nbsp; &nbsp;
+                <a
+                  class="nav-link active"
+                  aria-current="page"
+                  href={`../profile#access_token=${token}&token_type=Bearer&expires_in=3600`}
+                >
+                  <MdAccountCircle /> Profile
+                </a>
+                &nbsp; &nbsp;
+                <a
+                  class="nav-link active"
+                  aria-current="page"
+                  href={`../connections#access_token=${token}&token_type=Bearer&expires_in=3600`}
+                >
+                  <MdAddReaction /> Connections
+                </a>
+                &nbsp; &nbsp;
+                <a
+                  class="nav-link active"
+                  aria-current="page"
+                  //onClick={MessageForm}
+                  href={`/home/chat#access_token=${token}&token_type=Bearer&expires_in=3600`}
+                >
+                  <MdChat /> Chat
+                </a>
+                &nbsp; &nbsp;
+                <a
+                  class="nav-link active"
+                  aria-current="page"
+                  onClick={signOut}
+                  href="/#"
+                >
+                  <MdCompareArrows /> Sign out
+                </a>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+      <div class="itemchat">
+        <ChatEngine
+            height="100vh"
+            projectID="5c486811-745b-4945-a151-83eb47fcece8"
+            userName="obonilla"
+            userSecret="1234"
+        /> 
+      </div>
+      
+    </div>
+        );
+
 }
 
 export default ChatForm;
