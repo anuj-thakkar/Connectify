5a2aa6c5f29a064d8c5ce34c35365da662bf5e91
Rama Deshpande <rama@deshpandez.com>
2022-11-30 22:39:51 -0500

    new create post box

--- frontend/src/components/CreatePosts.jsx
+++ frontend/src/components/CreatePosts.jsx
@@ -1,43 +1,68 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { createPost } from '../services/usersService';
-const CreatPost = ()=>{
+import { useStateProvider } from "../utils/StateProvider";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const CreatPost = () => {
     const [title, setTitle] = useState("");
     const [body, setBody] = useState("");
 
     const postDetails = (e) => {
         if (title !== "" && body !== "") {
-        createPost(localStorage.getItem('username'), title, body);
+            createPost(localStorage.getItem('username'), title, body);
         }
     }
-    return(
-       <div className="card input-filed"
-       style={{
-           margin:"10px auto",
-           maxWidth:"500px",
-           padding:"5px",
-           textAlign:"center"
-       }}
-       >
-           <input 
-           type="text"
-            placeholder="title"
-            value={title}
-            onChange={(e)=>setTitle(e.target.value)}
-            />
-           <input
-            type="text"
-             placeholder="body"
-             value={body}
-            onChange={(e)=>setBody(e.target.value)}
-             />
-            <button className="btn waves-effect waves-light #64b5f6 blue darken-1"
-            onClick={()=>postDetails()}
-            >
-                Submit post
-            </button>
+    const [{ token, userInfo }] = useStateProvider();
+  var [limitReached, isLimitReached] = useState(false)
+  setInterval(function (){
+    var counter = document.getElementById('counter')
+    var input = document.getElementById('input')
+    var input_v = input.value;
+    var input_vl = input_v.length;
+    counter.innerHTML = input_vl;
+    if (counter.innerHTML >= 160) {
+      isLimitReached(true)
+    }
+    else {
+      isLimitReached(false)
+    }
+    
+},0)
 
-       </div>
-   )
+  console.log(userInfo);
+    return (
+        
+        <div className="item1">
+            <div className="shareWrapper">
+                <div className="shareTop">
+                <img className="shareProfileImg" src={userInfo ? userInfo.imagesUrl : null} alt="" />
+                &nbsp;
+                <input
+                    type="input"
+                    placeholder="title"
+                    classname="shareInput"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    maxLength={160}
+                />
+                &nbsp;&nbsp;&nbsp;
+                <input
+                    type="text"
+                    classname="shareInput"
+                    placeholder="body"
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                />
+                &nbsp;&nbsp;&nbsp;
+                <button className="btn btn-outline-success"
+                    onClick={() => postDetails()}
+                >
+                    Submit Post
+                </button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 
